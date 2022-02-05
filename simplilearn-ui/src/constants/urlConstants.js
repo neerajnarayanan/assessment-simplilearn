a566aa7d9392728103fc9301e49ed975afe0670b
neerajnarayanan <neerajperiya@gmail.com>
2022-02-05 14:15:14 +0530

    url const compose updated

--- simplilearn-ui/src/constants/urlConstants.js
+++ simplilearn-ui/src/constants/urlConstants.js
@@ -1,5 +1,5 @@
 export const APPCONSTANTS = {
-    BASE_URL: 'http://3.108.36.172:8000/',
+    BASE_URL: 'http://3.108.36.172:8000/', 
     LOGIN_API: 'auth/login',
     ADD_USER_API: 'auth/signup'
   }
